d04271fc4ba0942906180ebae82b0b863e7396db
Marco Servio Almeida Capanema <marco.capanema@lg.com.br>
2025-07-10 12:03:44 -0300

    feat: adding migration from tags

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -8,4 +8,11 @@ public class CashFlowDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
